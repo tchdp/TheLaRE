3d758b37e6e2424ee30295fcb2f840db4786d824
marcia <mrivera05@manhattan.edu>
2017-07-29 16:48:34 -0400

    first committ

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -1,7 +1,6 @@
-console.log ('Vegetables are very healthy');
-alert ("Life is amazing");
 var vrHomePage = vRViewPlayer('#vr-homepage', {
     image: 'mei.jpg',
     is_stereo: false,
     is_autopan_off: true,
 });
+window.addEventListener('load', vrHomePage);
