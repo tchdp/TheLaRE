f46dad032dfb3103d8e9b7d3d3064cd5543881e3
Doge Lounge Pairing Station <msela_engineering_dl@amplify.com>
2017-08-05 15:35:02 -0400

    changed yaw and pitch

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -7,8 +7,8 @@ window.addEventListener('load', vrHomePage);
 
 vrHomePage.on('ready', function(events){
   vrHomePage.addHotspot('creating-hotspot-1', {
-    pitch: -20,
-    yaw: -100,
+    pitch: -18,
+    yaw: -98,
     radius: 0.10,
     distance: 1
   });
