b21990e6ee4fa8837a4392f5075d4e04912115e2
Danny Perez <perez.dp@gmail.com>
2017-08-12 15:00:59 -0400

    Update webVRDayTwo.js

--- webVRDayTwo.js
+++ webVRDayTwo.js
@@ -19,7 +19,7 @@ vrHomePage.on('click', function(event){
   if (event.id == 'creating-hotspot-1'){
     vrHomePage.setContent({
         image: 'pic2.jpg',
-        is_stereo: true
+        is_stereo: false
     });
   }
 });
